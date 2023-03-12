cd18b091ee8f2969c5e0c545c413630783c3b21c
YuricgSilva <111657218+YuricgSilva@users.noreply.github.com>
2023-03-12 15:35:03 -0300

    Verbas Rescisórias V1.0.1

--- Verbas_Recisorias/VerbasRecisoias.c
+++ Verbas_Recisorias/VerbasRecisoias.c
@@ -2,52 +2,47 @@
 #include <gconio.h>
  
 void main(){
- 
-    printf("\t***********************************************************************\n");
-    printf("\t*                          VERBAS RESCISÓRIAS                         *\n");
-    printf("\t***********************************************************************\n");
-    printf("\t*                                 MENU                                *\n");
-    printf("\t*                                                                     *\n");
-    printf("\t* 1-Demissão Sem Justa Calsa                    4-Acordo Entre Partes *\n");
-    printf("\t* 2-Demissão Com Justa Calsa                    5-Demissão Consensual *\n");
-    printf("\t* 3-Pedido de Demissão Pelo Funcionário         0-Sair do Programa    *\n");
-    printf("\t*                                                                     *\n");
-    printf("\t***********************************************************************\n\n");
- 
-    char ch;
- 
-    printf("\tInforme a Opção Desejada: ");
-    ch = getchar();
+
+   char opcao;
  
  //-----Menu---------------------------------------------------------------------------------
+   printf("\t***********************************************************************\n");
+   printf("\t*                          VERBAS RESCISÓRIAS                         *\n");
+   printf("\t***********************************************************************\n");
+   printf("\t*                                 MENU                                *\n");
+   printf("\t*                                                                     *\n");
+   printf("\t* 1-Demissão Sem Justa Calsa                    4-Acordo Entre Partes *\n");
+   printf("\t* 2-Demissão Com Justa Calsa                    5-Demissão Consensual *\n");
+   printf("\t* 3-Pedido de Demissão Pelo Funcionário         0-Sair do Programa    *\n");
+   printf("\t*                                                                     *\n");
+   printf("\t***********************************************************************\n\n");
+   
+   printf("\tInforme a Opção Desejada: ");
+   opcao = getchar();
 
-    switch(ch){
-       case '1':
-          printf("\tCálculo função 1\n");
-          printf("FIM");
-          break;
-       case '2':
-          printf("\tCálculo função 2\n");
-          printf("FIM");
-          break;
-       case '3':
-          printf("\tCálculo função 3\n");
-          printf("FIM");
-          break;
-       case '4':
-          printf("\tCálculo função 4\n");
-          printf("FIM");
-          break;
-       case '5':
-          printf("\tCálculo função 5\n");
-          printf("FIM");
-          break;
-       case '0':
-          printf("\tSaio do Programa!!\n");
-          printf("FIM");
-          break;
-       default:
-          textcolor(RED);
-          printf("\tERRO: Opção Não Cadastrada!!\n");
-    }
+   switch(opcao){
+      case '1':
+         printf("\tCálculo função 1\n");
+         printf("\tFIM");
+         break;
+      case '2':
+         printf("\tCálculo função 2\n");
+         printf("\tFIM");
+         break;
+      case '3':
+         printf("\tCálculo função 3\n");
+         printf("\tFIM");
+         break;
+      case '4':
+         printf("\tCálculo função 4\n");
+         printf("\tFIM");
+         break;
+      case '5':
+         printf("\tCálculo função 5\n");
+         printf("\tFIM");
+         break;
+      default:
+         textcolor(RED);
+         printf("\tERRO: Opção Não Cadastrada!!\n");
+   }
 }
