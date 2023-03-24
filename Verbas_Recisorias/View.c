82e4094aa3c774c5f9d66035ad07b3d878519c8b
YuricgSilva <111657218+YuricgSilva@users.noreply.github.com>
2023-03-23 22:32:42 -0300

    Verbas_RescisoriasV1.0.4

--- Verbas_Recisorias/View.c
+++ Verbas_Recisorias/View.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 #include<gconio.h>
  
-int menu();
- 
+int viewMenu();
+
+int viewOpcao1();
+int viewOpcao2();
+int viewOpcao3();
+int viewOpcao4();
+int viewOpcao5();
+
+int viewVoltarMenu();
+
+//--Mneu Main-----------------------
 void main(){
     clrscr();
-    menu();
+    viewMenu();
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
  
-int menu(){
+int viewMenu(){
 //--Barra Superior------------------
     int x[10],y[10];
     for(x[0]=1;x[0]<73;x[0]++){
@@ -56,7 +66,195 @@ int menu(){
     printf("5-Demissão Consensual");
     gotoxy(50,9);
     printf("0-Sair do Programa");
+    return 0;
+}
+
+//--Screen Option 1-----------------
+int viewOpcao1(){
+//--Barra Superior------------------
+    int x[10],y[10];
+    for(x[0]=1;x[0]<73;x[0]++){
+        gotoxy(x[0],1);
+        printf("*");
+    }
+  
+//--Barra Inferior------------------
+    for(x[1]=1;x[1]<73;x[1]++){
+        gotoxy(x[1],15);
+        printf("*");
+    }
+//--Barra Do Meio-------------------
+    for(x[2]=2;x[2]<73;x[2]++){
+        gotoxy(x[2],3);
+        printf("*");
+    }
+  
+//--Barra Lateral Esquerda----------
+    for(y[0]=2;y[0]<15;y[0]++){
+        gotoxy(1,y[0]);
+        printf("*");
+    }
  
-    printf("\n\n\n\n\n\n\n");
+//--Barra Lateral Direita-----------   
+    for(y[1]=2;y[1]<15;y[1]++){
+        gotoxy(72,y[1]);
+        printf("*");
+    }
+//--Titulo--------------------------
+    gotoxy(25,2);
+    printf("Demissão Sem Justa Calsa");
+
+    return 0;
+}
+
+//--Screen Option 2-----------------
+int viewOpcao2(){
+//--Barra Superior------------------
+    int x[10],y[10];
+    for(x[0]=1;x[0]<73;x[0]++){
+        gotoxy(x[0],1);
+        printf("*");
+    }
+  
+//--Barra Inferior------------------
+    for(x[1]=1;x[1]<73;x[1]++){
+        gotoxy(x[1],15);
+        printf("*");
+    }
+//--Barra Do Meio-------------------
+    for(x[2]=2;x[2]<73;x[2]++){
+        gotoxy(x[2],3);
+        printf("*");
+    }
+  
+//--Barra Lateral Esquerda----------
+    for(y[0]=2;y[0]<15;y[0]++){
+        gotoxy(1,y[0]);
+        printf("*");
+    }
+ 
+//--Barra Lateral Direita-----------   
+    for(y[1]=2;y[1]<15;y[1]++){
+        gotoxy(72,y[1]);
+        printf("*");
+    }
+//--Titulo--------------------------
+    gotoxy(25,2);
+    printf("Demissão Com Justa Calsa ");
+
+    return 0;
+}
+
+//--Screen Option 3-----------------
+int viewOpcao3(){
+//--Barra Superior------------------
+    int x[10],y[10];
+    for(x[0]=1;x[0]<73;x[0]++){
+        gotoxy(x[0],1);
+        printf("*");
+    }
+  
+//--Barra Inferior------------------
+    for(x[1]=1;x[1]<73;x[1]++){
+        gotoxy(x[1],15);
+        printf("*");
+    }
+//--Barra Do Meio-------------------
+    for(x[2]=2;x[2]<73;x[2]++){
+        gotoxy(x[2],3);
+        printf("*");
+    }
+  
+//--Barra Lateral Esquerda----------
+    for(y[0]=2;y[0]<15;y[0]++){
+        gotoxy(1,y[0]);
+        printf("*");
+    }
+ 
+//--Barra Lateral Direita-----------   
+    for(y[1]=2;y[1]<15;y[1]++){
+        gotoxy(72,y[1]);
+        printf("*");
+    }
+//--Titulo--------------------------
+    gotoxy(19,2);
+    printf("Pedido de Demissão Pelo Funcionário");
+
+    return 0;
+}
+
+//--Screen Option 4-----------------
+int viewOpcao4(){
+//--Barra Superior------------------
+    int x[10],y[10];
+    for(x[0]=1;x[0]<73;x[0]++){
+        gotoxy(x[0],1);
+        printf("*");
+    }
+  
+//--Barra Inferior------------------
+    for(x[1]=1;x[1]<73;x[1]++){
+        gotoxy(x[1],15);
+        printf("*");
+    }
+//--Barra Do Meio-------------------
+    for(x[2]=2;x[2]<73;x[2]++){
+        gotoxy(x[2],3);
+        printf("*");
+    }
+  
+//--Barra Lateral Esquerda----------
+    for(y[0]=2;y[0]<15;y[0]++){
+        gotoxy(1,y[0]);
+        printf("*");
+    }
+ 
+//--Barra Lateral Direita-----------   
+    for(y[1]=2;y[1]<15;y[1]++){
+        gotoxy(72,y[1]);
+        printf("*");
+    }
+//--Titulo--------------------------
+    gotoxy(27,2);
+    printf("Acordo Entre Partes");
+
+    return 0;
+}
+
+//--Screen Option 5-----------------
+int viewOpcao5(){
+//--Barra Superior------------------
+    int x[10],y[10];
+    for(x[0]=1;x[0]<73;x[0]++){
+        gotoxy(x[0],1);
+        printf("*");
+    }
+  
+//--Barra Inferior------------------
+    for(x[1]=1;x[1]<73;x[1]++){
+        gotoxy(x[1],15);
+        printf("*");
+    }
+//--Barra Do Meio-------------------
+    for(x[2]=2;x[2]<73;x[2]++){
+        gotoxy(x[2],3);
+        printf("*");
+    }
+  
+//--Barra Lateral Esquerda----------
+    for(y[0]=2;y[0]<15;y[0]++){
+        gotoxy(1,y[0]);
+        printf("*");
+    }
+ 
+//--Barra Lateral Direita-----------   
+    for(y[1]=2;y[1]<15;y[1]++){
+        gotoxy(72,y[1]);
+        printf("*");
+    }
+//--Titulo--------------------------
+    gotoxy(27,2);
+    printf("Demissão Consensual");
+
     return 0;
 }
